878afe87387f50302b56a9e862340657114fe502
Ravi Rajput <raviiii8126@gmail.com>
2026-06-02 22:08:17 +0530

    JWT login authentication completed

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -3,8 +3,17 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.user import User
-from app.schemas import UserCreate
-from app.services.security import hash_password
+
+from app.schemas import UserCreate, UserLogin
+
+from app.services.security import (
+    hash_password,
+    verify_password
+)
+
+from app.services.jwt_service import (
+    create_access_token
+)
 
 router = APIRouter()
 
@@ -33,4 +42,38 @@ def register(
 
     return {
         "message": "User registered successfully"
+    }
+@router.post("/login")
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    db_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if not db_user:
+        return {
+            "message": "Invalid email"
+        }
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        return {
+            "message": "Invalid password"
+        }
+
+    token = create_access_token(
+        {
+            "sub": db_user.email
+        }
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
     }
